ed7165f7cea40372fdc7b2a44459ad642bc83506
jocyanno <jocyannovittor@hotmail.com>
2023-07-19 22:39:11 -0300

    commit -m up servidor

--- servidor.js
+++ servidor.js
@@ -1,8 +1,30 @@
 const net = require('net');
-
 const serverPort = 8070; // Porta do servidor
 let nextClientId = 1; // Variável para controlar os identificadores dos clientes
 const clients = []; // Array para armazenar os clientes conectados
+
+const processRequest = async (socket, clientId) => {
+  try {
+    socket.on('data', (data) => {
+      console.log(`Resposta enviada do cliente ${clientId}: ${data.toString().trim()}`);
+    });
+
+    socket.on('error', (err) => {
+      console.error(`Erro ao processar a solicitação do cliente ${clientId}:, err`);
+    });
+
+    // Opcionalmente, você pode adicionar um evento 'end' para tratar a desconexão do cliente
+    socket.on('end', () => {
+      console.log(`Cliente ${clientId} desconectado.`);
+      const index = clients.findIndex((client) => client.id === clientId);
+      if (index !== -1) {
+        clients.splice(index, 1); // Remove o cliente da lista de clientes conectados
+      }
+    });
+  } catch (err) {
+    console.error(`Erro ao processar a solicitação do cliente ${clientId}:, err`);
+  }
+};
 
 const server = net.createServer(function (socket) {
   const clientId = nextClientId++; // Atribui um identificador único ao cliente conectado
@@ -10,17 +32,7 @@ const server = net.createServer(function (socket) {
 
   console.log(`Cliente ${clientId} conectado.`);
 
-  socket.on('data', function (data) {
-    console.log(`Resposta enviada do cliente ${clientId}: ` + data.toString().trim());
-  });
-
-  socket.on('end', function () {
-    console.log(`Cliente ${clientId} desconectado.`);
-    const index = clients.findIndex((client) => client.id === clientId);
-    if (index !== -1) {
-      clients.splice(index, 1); // Remove o cliente da lista de clientes conectados
-    }
-  });
+  processRequest(socket, clientId);
 });
 
 server.listen(serverPort, function () {
