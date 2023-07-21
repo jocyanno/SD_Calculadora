db3905ab1651be6e56081f8ae8ac06fb13f13b81
jocyanno <jocyannovittor@hotmail.com>
2023-07-20 22:41:39 -0300

    bug duplicada no cliente

--- cliente.js
+++ cliente.js
@@ -24,7 +24,7 @@ client.connect(serverPort, serverIP, function () {
   });
 
   client.on('data', function (data) {
-    console.log('Valor recebido pelo servidor:', data.toString().trim());
+    console.log('Resposta do servidor:', data.toString().trim());
   });
 
   client.on('close', function () {
@@ -35,16 +35,23 @@ client.connect(serverPort, serverIP, function () {
 
 client.on('error', function (err) {
   console.log('Não foi possível conectar ao servidor. Os cálculos serão realizados localmente.');
+
   console.log('Escolha uma operação:');
   console.log('1 - Área de uma casa');
   console.log('2 - Área de um círculo');
   console.log('3 - O poder do seu soco no espaço, na velocidade da luz');
-  rl.on('line', function (operacao) {
+  
+  rl.removeAllListeners('line');
 
+  const rlLocal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rlLocal.on('line', function (operacao) {
     switch (operacao) {
       case '1':
-        rl.question('Qual o comprimento da casa? ', function (comprimentoCasa) {
-          rl.question('Qual a largura da casa? ', function (larguraCasa) {
+        rlLocal.question('Qual o comprimento da casa? ', function (comprimentoCasa) {
+          rlLocal.question('Qual a largura da casa? ', function (larguraCasa) {
             const areaCasa = comprimentoCasa * larguraCasa;
             console.log(`A área da casa é: ${areaCasa}`);
           });
@@ -52,14 +59,14 @@ client.on('error', function (err) {
         break;
 
       case '2':
-        rl.question('Qual o raio do círculo? ', function (raioCirculo) {
+        rlLocal.question('Qual o raio do círculo? ', function (raioCirculo) {
           const areaCirculo = Math.PI * raioCirculo * raioCirculo;
           console.log(`A área do círculo é: ${areaCirculo}`);
         });
         break;
 
       case '3':
-        rl.question('Qual a massa do seu soco? ', function (massaSoco) {
+        rlLocal.question('Qual a massa do seu soco? ', function (massaSoco) {
           const poderSoco = massaSoco * Math.pow(299792458, 2);
           console.log(`Seu soco teria o poder de ${poderSoco} joules, poderia causar uma catástrofe!`);
         });
--- servidor.js
+++ servidor.js
@@ -3,38 +3,79 @@ const serverPort = 8070;
 let nextClientId = 1;
 const clients = [];
 
+// Estados possíveis do cliente
+const ClientState = {
+  IDLE: 'IDLE', // O cliente não está realizando nenhum cálculo
+  AWAITING_LENGTH: 'AWAITING_LENGTH', // O cliente está aguardando o envio do comprimento da casa
+  AWAITING_WIDTH: 'AWAITING_WIDTH', // O cliente está aguardando o envio da largura da casa
+  AWAITING_RADIUS: 'AWAITING_RADIUS', // O cliente está aguardando o envio do raio do círculo
+  AWAITING_MASS: 'AWAITING_MASS', // O cliente está aguardando o envio da massa do soco
+};
+
 const processRequest = (socket, clientId, data) => {
+  const client = clients.find((client) => client.id === clientId);
+  if (!client) {
+    console.error(`Cliente ${clientId} não encontrado.`);
+    return;
+  }
+
+  const { currentState } = client;
   try {
-    switch (data) {
-      case '1':
-        socket.write("Qual o comprimento da casa?\n");
-        socket.once("data", (comprimentoCasa) => {
-          comprimentoCasa = parseFloat(comprimentoCasa);
-          socket.write("Qual a largura da casa?\n");
-          socket.once("data", (larguraCasa) => {
-            larguraCasa = parseFloat(larguraCasa);
-            const areaCasa = comprimentoCasa * larguraCasa;
-            socket.write(`A área da casa é: ${areaCasa}\n`);
-          });
-        });
+    switch (currentState) {
+      case ClientState.IDLE:
+        // O cliente está enviando a operação desejada
+        const operacao = data.toString().trim();
+        switch (operacao) {
+          case '1':
+            client.currentState = ClientState.AWAITING_LENGTH;
+            socket.write("Qual o comprimento da casa?\n");
+            break;
+          case '2':
+            client.currentState = ClientState.AWAITING_RADIUS;
+            socket.write("Qual o raio do círculo?\n");
+            break;
+          case '3':
+            client.currentState = ClientState.AWAITING_MASS;
+            socket.write("Qual a massa do seu soco?\n");
+            break;
+          default:
+            socket.write('Operação inválida!\n');
+            break;
+        }
         break;
 
-      case '2':
-        socket.write("Qual o raio do círculo?\n");
-        socket.once("data", (raioCirculo) => {
-          raioCirculo = parseFloat(raioCirculo);
-          const areaCirculo = Math.PI * raioCirculo * raioCirculo;
-          socket.write(`A área do círculo é: ${areaCirculo}\n`);
-        });
+      case ClientState.AWAITING_LENGTH:
+        // O cliente enviou o comprimento da casa, agora aguardamos a largura
+        client.comprimentoCasa = parseFloat(data);
+        client.currentState = ClientState.AWAITING_WIDTH;
+        socket.write("Qual a largura da casa?\n");
         break;
 
-      case '3':
-        socket.write("Qual a massa do seu soco?\n");
-        socket.once("data", (massaSoco) => {
-          massaSoco = parseFloat(massaSoco);
-          const poderSoco = massaSoco * Math.pow(299792458, 2);
-          socket.write(`Seu soco teria o poder de ${poderSoco} joules, poderia causar uma catástrofe!\n`);
-        });
+      case ClientState.AWAITING_WIDTH:
+        // O cliente enviou a largura da casa, podemos calcular a área
+        const larguraCasa = parseFloat(data);
+        const areaCasa = client.comprimentoCasa * larguraCasa;
+        console.log(`Cliente ${clientId} enviou comprimento: ${client.comprimentoCasa}, largura: ${larguraCasa}`);
+        socket.write(`A área da casa é: ${areaCasa}\n`);
+        client.currentState = ClientState.IDLE;
+        break;
+
+      case ClientState.AWAITING_RADIUS:
+        // O cliente enviou o raio do círculo, podemos calcular a área
+        const raioCirculo = parseFloat(data);
+        const areaCirculo = Math.PI * raioCirculo * raioCirculo;
+        console.log(`Cliente ${clientId} enviou raio do círculo: ${raioCirculo}`);
+        socket.write(`A área do círculo é: ${areaCirculo}\n`);
+        client.currentState = ClientState.IDLE;
+        break;
+
+      case ClientState.AWAITING_MASS:
+        // O cliente enviou a massa do soco, podemos calcular o poder do soco
+        const massaSoco = parseFloat(data);
+        const poderSoco = massaSoco * Math.pow(299792458, 2);
+        console.log(`Cliente ${clientId} enviou massa do soco: ${massaSoco}`);
+        socket.write(`Seu soco teria o poder de ${poderSoco} joules, poderia causar uma catástrofe!\n`);
+        client.currentState = ClientState.IDLE;
         break;
 
       default:
@@ -48,17 +89,17 @@ const processRequest = (socket, clientId, data) => {
 
 const server = net.createServer(function (socket) {
   const clientId = nextClientId++;
-  clients.push({ id: clientId, socket });
+  clients.push({ id: clientId, socket, currentState: ClientState.IDLE });
 
   console.log(`Cliente ${clientId} conectado.`);
 
   socket.on("data", (data) => {
     console.log(`Requisição recebida do cliente ${clientId}: ${data.toString().trim()}`);
-    processRequest(socket, clientId, data.toString().trim());
+    processRequest(socket, clientId, data);
   });
 
   socket.on("error", (err) => {
-    console.error(`Erro ao processar a solicitação do cliente ${clientId}:, err`);
+    console.error(`Erro ao processar a solicitação do cliente ${clientId}:`, err);
   });
 
   socket.on("end", () => {
